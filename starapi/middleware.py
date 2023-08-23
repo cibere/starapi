1d875b5d6a6d6c85ef1c4cad8f04aa4801732b22
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-08-23 12:11:33 -0700

    middleware split http and ws

--- starapi/middleware.py
+++ starapi/middleware.py
@@ -1,18 +1,29 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from .requests import BaseRequest
+    from .requests import Request, WebSocket
 
 __all__ = ("BaseMiddleware",)
 
 
-class BaseMiddleware(ABC):
-    @abstractmethod
-    async def __call__(self, request: BaseRequest) -> None:
+class BaseMiddleware:
+    async def handle_http(self, request: Request) -> None:
         ...
+
+    async def handle_ws(self, ws: WebSocket) -> None:
+        ...
+
+    async def handle(self, connection: Request | WebSocket) -> None:
+        ...
+
+    async def __call__(self, connection: Request | WebSocket) -> None:
+        if connection._type == 'http':
+            await self.handle_http(connection)
+        else:
+            await self.handle_ws(connection)
+        await self.handle(connection)
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__}>"
